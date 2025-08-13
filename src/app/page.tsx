ddc0ab1795771ce5e83489e266b4558e08d39b84
Rianhahaha <triandiapriliio@gmail.com>
2025-08-13 22:17:32 +0700

    feedback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Header from "./components/header";
 import Bg from "@/component/bg";
 import Hero from "./components/home/hero";
-import Section2 from "./components/home/section2";
+
 
 export default function Home() {
   return (
@@ -9,7 +9,6 @@ export default function Home() {
   <Bg/>
   <Header/>
   <Hero/>
-  <Section2/>
   </>
   );
 }
